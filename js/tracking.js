0261acdad2248a40b635e71f2bfdd4b97bd3efdb
Devyn Myers <dmyers@webstaurantstore.com>
2025-07-11 15:20:42 -0400

    added sync logic with ADO

--- js/tracking.js
+++ js/tracking.js
@@ -128,28 +128,16 @@ function getDateFromString(text) {
  * @returns {Object<string, number>}
  */
 function getSummaryCollection(text) {
-    var lines = text.split('\n');
-
     var summaryCollection = {};
-    var previousTime = null;
-    for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        var segments = line.split(" - ");
-        var timestamp = getDateFromString(segments[0]);
+    var workItems = getWorkItems(text);
 
-        var deltaTimeMinutes = 0;
-        if (previousTime) {
-            var diffMiliseconds = timestamp - previousTime;
-            deltaTimeMinutes = Math.floor(diffMiliseconds / (1000 * 60));
-        }
-        previousTime = timestamp;
-
-        var keyWords = getAllKeyWords(segments[1]);
-        for (var word of keyWords) {
-            if (word in summaryCollection) {
-                summaryCollection[word] += deltaTimeMinutes;
+    for (const key in workItems) {
+        const tasks = workItems[key];
+        for (const task of tasks) {
+            if (key in summaryCollection) {
+                summaryCollection[key] += task.taskTimeMinutes;
             } else {
-                summaryCollection[word] = deltaTimeMinutes;
+                summaryCollection[key] = task.taskTimeMinutes;
             }
         }
     }
@@ -186,6 +174,271 @@ function getSummaryText(summaryCollection){
     summaryText += `Total Meeting Time: ${getTimeSpentString(totalMeetingMinutes)}<br/>`;
 
     return summaryText;
+}
+
+
+/**
+ * @returns {{ [workItemNumber: number]: { taskName: string, taskTimeMinutes: number }[] }}
+ */
+function getWorkItems(text) {
+    const lines = text.split('\n');
+    const workItems = {};
+
+    var previousTime = null;
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        var segments = line.split(" - ");
+        var timestamp = getDateFromString(segments[0]);
+
+        var deltaTimeMinutes = 0;
+        if (previousTime) {
+            var diffMiliseconds = timestamp - previousTime;
+            deltaTimeMinutes = Math.floor(diffMiliseconds / (1000 * 60));
+        }
+        previousTime = timestamp;
+
+        var keyWord = getAllKeyWords(segments[1]).at(0);
+        if (!keyWord) {
+            continue;
+        }
+
+        const workItemNumber = parseInt(keyWord.replace("#", ""));
+        if(isNaN(workItemNumber)) {
+            continue;
+        }
+
+        const taskDescription = segments[1].replace(keyWord, "").trim();
+        
+        if(workItems[workItemNumber]) {
+            workItems[workItemNumber].push({
+                taskName: taskDescription,
+                taskTimeMinutes: deltaTimeMinutes
+            });
+        }
+        else {
+            workItems[workItemNumber] = [{
+                taskName: taskDescription,
+                taskTimeMinutes: deltaTimeMinutes
+            }];
+        }
+    }
+
+    return workItems;
+}
+
+async function submitChanges(){
+    /**
+     * @type {{ url: string, pat: string, org: string, project: string } | null}
+     */
+    const adoConfig = JSON.parse(localStorage.getItem('ado-config'));
+
+    if (!adoConfig) {
+        alert("Please configure ADO settings first.");
+        return;
+    }
+    
+    const text = document.getElementById('editor').value;
+    const workItemTaskCollection = getWorkItems(text);
+
+    console.log(workItemTaskCollection);
+
+    //Task time should be rounded from minutes to nearest 15 minutes, and then divided by 60
+    /** @type {{ parentWorkItemNumber: number, taskName: string, taskTimeRounded: number }[]} */
+    const workItemTasks = Object.keys(workItemTaskCollection).flatMap((workItemNumberKey) => {
+        const workItemNumber = parseInt(workItemNumberKey);
+        const tasks = workItemTaskCollection[workItemNumber];
+        return tasks.map(({ taskName, taskTimeMinutes }) => ({
+            parentWorkItemNumber: workItemNumber,
+            taskName,
+            taskTimeRounded: Math.round(taskTimeMinutes / 15) * 15 / 60
+        }))
+    });
+
+    const workItemDisplayText = workItemTasks.map(({ parentWorkItemNumber, taskName, taskTimeRounded }) => {
+        return `Work Item #${parentWorkItemNumber}: ${taskName} (${taskTimeRounded}h)`;
+    }).join("\n");
+
+    if(!confirm("you are about to submit the following tasks:\n" + workItemDisplayText))
+    {
+        return;
+    }
+
+    const workItemParentMap = new Map();
+
+    // Fetch each work item's parent from ADO
+    for (const { parentWorkItemNumber } of workItemTasks) {
+        if (!workItemParentMap.has(parentWorkItemNumber)) {
+            const parentWorkItem = await getWorkitem(parentWorkItemNumber);
+            console.log(parentWorkItem)
+            workItemParentMap.set(parentWorkItemNumber, parentWorkItem);
+        }
+    }
+
+    //Loop over work items and get all child tasks
+    for(const parentWorkItem of workItemParentMap.values()) {
+        /** @type {number[]} */
+        const childTasks = (parentWorkItem.relations ?? [])
+            .filter(relation => relation.rel === "System.LinkTypes.Hierarchy-Forward")
+            .map(relation => {
+                const urlParts = relation.url.split('/');
+                return parseInt(urlParts[urlParts.length - 1]);
+            });
+
+        console.log("child tasks for work item #" + parentWorkItem.id + ": " + childTasks);
+
+        for(const childTaskId of childTasks) {
+            const childTask = await getWorkitem(childTaskId);
+            console.log(childTask);
+            
+            if(parentWorkItem.children){
+                parentWorkItem.children.push(childTask);
+            }
+            else {
+                parentWorkItem.children = [childTask];
+            }
+        }
+    }
+
+    console.log("work item parent map: ", workItemParentMap);
+
+    for (const task of workItemTasks) {
+
+        //If the parent already has the child task, update the time
+        const parentWorkItem = workItemParentMap.get(task.parentWorkItemNumber);
+
+        const childTask = parentWorkItem.children?.find(child => {
+            const isMatchingTitle = child.fields["System.Title"] === task.taskName;
+            const isAssignedToMe = child.fields["System.AssignedTo"]?.uniqueName === adoConfig.username;
+            return isMatchingTitle && isAssignedToMe;
+        });
+
+        if (childTask) {
+            const completedWork = childTask.fields["Microsoft.VSTS.Scheduling.CompletedWork"] || 0;
+            const newCompletedWork = completedWork + task.taskTimeRounded;
+            const fields = [
+                {
+                    "op": "add",
+                    "path": "/fields/Microsoft.VSTS.Scheduling.CompletedWork",
+                    "value": newCompletedWork
+                }
+            ];
+
+            const updatedTask = await updateWorkItem(childTask.id, fields);
+            console.log(`Updated task #${childTask.id}`, updatedTask);
+        }
+
+        //If the parent does not have the child task, create it
+        else {
+            const newTask = await createTaskWorkItem(task.parentWorkItemNumber, task.taskName, task.taskTimeRounded);
+            console.log(`Created new task #${newTask.id}`, newTask);
+        }
+
+    }
+
+}
+
+async function getWorkitem(workItemId) {
+    const adoConfig = JSON.parse(localStorage.getItem('ado-config'));
+    const adoUrl = adoConfig.url;
+    const adoPat = adoConfig.pat;
+    const adoOrg = adoConfig.org;
+    const adoProject = adoConfig.project;
+
+    const workItemResponse = await fetch(`${adoUrl}/${adoOrg}/${adoProject}/_apis/wit/workitems/${workItemId}?api-version=7.1&$expand=relations`, 
+    {
+        headers: 
+        {
+            'Authorization': `Basic ${btoa(`:${adoPat}`)}`
+        }
+    })
+
+    return await workItemResponse.json();
+}
+
+async function updateWorkItem(workItemId, fields) {
+    const adoConfig = JSON.parse(localStorage.getItem('ado-config'));
+    const adoUrl = adoConfig.url;
+    const adoPat = adoConfig.pat;
+    const adoOrg = adoConfig.org;
+    const adoProject = adoConfig.project;
+
+    const response = await fetch(`${adoUrl}/${adoOrg}/${adoProject}/_apis/wit/workitems/${workItemId}?api-version=7.1`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json-patch+json',
+            'Authorization': `Basic ${btoa(`:${adoPat}`)}`
+        },
+        body: JSON.stringify(fields)
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error updating work item: ${response.statusText}`);
+    }
+
+    return await response.json();
+}
+
+async function createTaskWorkItem(parentWorkItemId, taskName, taskTimeRounded) {
+    const adoConfig = JSON.parse(localStorage.getItem('ado-config'));
+    const adoUrl = adoConfig.url;
+    const adoPat = adoConfig.pat;
+    const adoOrg = adoConfig.org;
+    const adoProject = adoConfig.project;
+    const adoUsername = adoConfig.username;
+
+    const fields = [
+        {
+            "op": "add",
+            "path": "/fields/System.Title",
+            "value": taskName
+        },
+        {
+            "op": "add",
+            "path": "/fields/Microsoft.VSTS.Scheduling.CompletedWork",
+            "value": taskTimeRounded
+        },
+        {
+            "op": "add",
+            "path": "/relations/-",
+            "value": {
+                "rel": "System.LinkTypes.Hierarchy-Reverse",
+                "url": `${adoUrl}/${adoOrg}/${adoProject}/_apis/wit/workitems/${parentWorkItemId}`,
+                "attributes": {
+                    "comment": "Making a new link for the dependency"
+                }
+            }
+        },
+        {
+            "op": "add",
+            "path": "/fields/System.AssignedTo",
+            "value": adoUsername
+        }
+    ];
+
+    let response = await fetch(`${adoUrl}/${adoOrg}/${adoProject}/_apis/wit/workitems/$Task?api-version=7.1&$expand=relations`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json-patch+json',
+            'Authorization': `Basic ${btoa(`:${adoPat}`)}`
+        },
+        body: JSON.stringify(fields)
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error creating work item: ${response.statusText}`);
+    }
+
+    //Now immediately update the work item to done
+    const createdWorkItem = await response.json();
+    const doneFields = [
+        {
+            "op": "add",
+            "path": "/fields/System.State",
+            "value": "Done"
+        }
+    ];
+
+    return await updateWorkItem(createdWorkItem.id, doneFields);
 }
 
 function getTimeSpentString(minutes){
